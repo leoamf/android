c66c6fd45a0fa2117d3d8e5d1ab6303d213fc2ce
Leonardo Freitas <leonardo@lamfsistemas.com.br>
2017-08-11 21:55:51 -0300

    persistencia SQLite

--- ExemploBancoDeDados/app/src/main/java/posgraducao/lamfsistemas/com/br/exemplobancodedados/MainActivity.java
+++ ExemploBancoDeDados/app/src/main/java/posgraducao/lamfsistemas/com/br/exemplobancodedados/MainActivity.java
@@ -1,13 +1,56 @@
 package posgraducao.lamfsistemas.com.br.exemplobancodedados;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.sql.SQLData;
 
 public class MainActivity extends AppCompatActivity {
-
+    MeuOpenHelper meuOpenHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        meuOpenHelper = new MeuOpenHelper(getApplicationContext());
+    }
+
+    public void getInfo(View v) {
+        SQLiteDatabase db = meuOpenHelper.getWritableDatabase();
+        String[] projection = {"name","ano"};
+        String table = "carros";
+
+        String selection = "name = ? and ano=?";
+        String[] selectionValues = {"Teste","1982"};
+
+        Cursor cursor = db.query(table, projection, selection, selectionValues, null, null, null);
+        String textoFinal ="Não acessou!";
+        if(cursor.moveToFirst()){
+            textoFinal="";
+            do{
+                textoFinal = textoFinal + " | " + cursor.getString(cursor.getColumnIndex("name"));
+                textoFinal = textoFinal + " | " +cursor.getString(cursor.getColumnIndex("ano"));
+
+            } while(cursor.moveToNext());
+        }
+
+        Toast.makeText(getApplicationContext(),textoFinal,Toast.LENGTH_LONG).show();
+    }
+
+    public void insert(View v){
+
+        SQLiteDatabase db = meuOpenHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name","Teste");
+        contentValues.put("ano","1982");
+
+        long idCarro = db.insert("carros",null,contentValues);
+
+        Toast.makeText(getApplicationContext(),String.valueOf(  idCarro),Toast.LENGTH_LONG).show();
     }
 }
--- ExemploBancoDeDados/app/src/main/java/posgraducao/lamfsistemas/com/br/exemplobancodedados/MeuOpenHelper.java
+++ ExemploBancoDeDados/app/src/main/java/posgraducao/lamfsistemas/com/br/exemplobancodedados/MeuOpenHelper.java
@@ -10,17 +10,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MeuOpenHelper extends SQLiteOpenHelper   {
 
-    public MeuOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final  int   DATABASE_VERSION=1;
+    private static final   String  DATABASE_NAME="lamfTesteBanco";
+
+    public MeuOpenHelper(Context context ) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table carros(id integer primary key, name TEXT, ano TEXT) ");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        //db.execSQL("alter table carros  ");
     }
 }
